9a1840b3716a08b5cd0f20ddcfb6ec89b027d196
armenkahriman-max <armenkahriman93@gmail.com>
2026-09-01 13:59:50 +0200

    store recipes with ef core

--- RecipeMatcher.Web/Program.cs
+++ RecipeMatcher.Web/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using RecipeMatcher.Web.Data;
+using RecipeMatcher.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
+    if (!db.Recipes.Any())
+    {
+        db.Recipes.AddRange(
+            new Recipe { Name = "Pancakes", PreparationMinutes = 20 },
+            new Recipe { Name = "Tomato Soup", PreparationMinutes = 30 }
+        );
+        db.SaveChanges();
+    }
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
